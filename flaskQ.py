54c6daab13dfdc274f5a49fd6a02b0d1d12ac915
19fi115 <19fi115@ms.dendai.ac.jp>
2021-11-19 14:54:34 +0900

    2021/11/19 課題

---
+++ flaskQ.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    dictionary = {"name": "六鹿 博紀", "age": 21, "mail": "hiroki-mutsuga@isl.im.dendai.ac.jp", "address": "東京都"}
+    flag = False
+
+    return render_template('flaskQ.html', dictionary = dictionary, flag = flag)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(port = 8000)
